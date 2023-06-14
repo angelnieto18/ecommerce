16b3beab1156d9e06456dbd4988774e8c378fbbf
angelnieto18 <angelnietomatamoros@gmail.com>
2023-06-13 22:10:48 -0400

    Rutas para cada producto agregadas

--- src/routes/product/Product.jsx
+++ src/routes/product/Product.jsx
@@ -1,37 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { getProducts } from '../../App';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Product = () => {
-  const [product, setProduct] = useState({});
+  const [singleProduct, setSingleProduct] = useState({ images: [] });
   const { productId } = useParams();
 
   useEffect(() => {
-    getProducts(`https://dummyjson.com/products/${productId}`).then((product) => {
-      setProduct(product);
-    });
+    fetch(`https://dummyjson.com/products/${productId}`)
+      .then((response) => response.json())
+      .then((product) => setSingleProduct(product));
   }, []);
+
+  useEffect(() => {
+    console.log(singleProduct);
+  }, [singleProduct]);
 
   return (
     <>
       <div className='product'>
         <div className='product__image-container'>
-          <img
-            className='product__image'
-            src={product.images === undefined ? product.thumbnail : product.images[0]}
-          />
+          <img src={singleProduct.images[0]} />
         </div>
         <div className='product__price-container'>
-          <h1 className='product__price'>${product.price}</h1>
+          <h2 className='product__price'>${singleProduct.price}</h2>
         </div>
         <div className='product__title-container'>
-          <h2 className='product__title'>{product.title}</h2>
-        </div>
-        <div className='product__description-container'>
-          <p className='product__description'>{product.description}</p>
-        </div>
-        <div className='product__brand-container'>
-          <h2 className='product__brand'>{product.brand}</h2>
+          <h3 className='product__title'>{singleProduct.title}</h3>
         </div>
       </div>
     </>
